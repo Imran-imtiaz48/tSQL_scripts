450c699358763f57c5828335943245a2236446c1
Imran Imtiaz <emraan.imtiaz48@gmail.com>
2024-11-07 14:25:15 +0400

    Update Dates_112.sql

--- Dates_112.sql
+++ Dates_112.sql
@@ -1,14 +1,23 @@
-Declare @today varchar(10);
-Declare @Yesterday varchar(10);
-Declare @LastBusinessDay varchar(10);
+DECLARE @today VARCHAR(10);
+DECLARE @yesterday VARCHAR(10);
+DECLARE @lastBusinessDay VARCHAR(10);
 
-Set @today = convert(varchar(10),  getdate() ,112);
-Set @Yesterday =convert(varchar(10),  DATEADD(dd,-1, getdate()) ,112);
+-- Set today's date in YYYYMMDD format
+SET @today = CONVERT(VARCHAR(10), GETDATE(), 112);
 
-SET @LastBusinessDay = convert(varchar(10),  DATEADD(DAY, CASE (DATEPART(WEEKDAY, getdate()) + @@DATEFIRST) % 7 
-                        WHEN 1 THEN -2 
-                        WHEN 2 THEN -3 
-                        ELSE -1 
-                    END, DATEDIFF(DAY, 0, getdate())) ,112);
+-- Set yesterday's date in YYYYMMDD format
+SET @yesterday = CONVERT(VARCHAR(10), DATEADD(DAY, -1, GETDATE()), 112);
 
-Select @today,@Yesterday,@LastBusinessDay
+-- Calculate the last business day (ignores weekends)
+SET @lastBusinessDay = CONVERT(VARCHAR(10), 
+    DATEADD(DAY, 
+        CASE 
+            WHEN DATEPART(WEEKDAY, GETDATE()) = 2 THEN -3  -- Monday
+            WHEN DATEPART(WEEKDAY, GETDATE()) = 1 THEN -2  -- Sunday
+            ELSE -1                                       -- Other weekdays
+        END, 
+        GETDATE()), 
+    112);
+
+-- Select the results
+SELECT @today AS Today, @yesterday AS Yesterday, @lastBusinessDay AS LastBusinessDay;
